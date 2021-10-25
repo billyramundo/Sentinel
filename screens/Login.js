d7a7f90f0bdb5d7ba0206a3ebf9c3cb032e3c77d
Sylvie Mason <sylviemason@Sylvies-MacBook-Pro.local>
2021-10-24 23:05:05 -0400

    setup connection between firebase and front end bug fixes required

--- screens/Login.js
+++ screens/Login.js
@@ -1,6 +1,34 @@
 import React, { useState } from "react";
 import { styles } from "../Styles";
 import logo from "../assets/logo.png";
+import { getDatabase, ref, onValue } from 'firebase/database';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDp3DdsqNfYJeCXIveh-7dDvnJhmudgdeE",
+  authDomain: "sentinel-a6249.firebaseapp.com",
+  databaseURL: "https://sentinel-a6249-default-rtdb.firebaseio.com",
+  projectId: "sentinel-a6249",
+  storageBucket: "sentinel-a6249.appspot.com",
+  messagingSenderId: "1069922297779",
+  appId: "1:1069922297779:web:2f883ac3957053cb80cdc6",
+  measurementId: "G-5WBYZYXC4J"
+};
+
+// Initialize Firebase
+const db = getDatabase(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 import {
   Text,
   View,
@@ -9,12 +37,20 @@ import {
   TextInput,
   Image,
 } from "react-native";
+
 let username = "";
 function Login({ navigation }) {
   const [user, setUsername] = useState("");
   const movePage = () => {
     username = user;
     navigation.navigate("Home");
+  };
+  const storeUser = (userName) => {
+    //const db = getDatabase();
+    const reference = ref(db, 'users/' + userName);
+    set(ref(db, 'users/' + username), {
+      password: "password",
+    });
   };
 
   return (
@@ -39,7 +75,7 @@ function Login({ navigation }) {
           onChangeText={(user) => setUsername(user)}
         />
         <TextInput style={styles.input} placeholder="Password" />
-        <TouchableOpacity style={styles.button} onPress={movePage}>
+        <TouchableOpacity style={styles.button} onPress={movePage, storeUser(username)}>
           <Text
             style={{
               color: "white",
