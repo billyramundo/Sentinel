1485ae9eaa050dce014aeadecc0c52f1834457d4
Berkley Nahum <berkley.nahum@duke.edu>
2021-10-24 22:51:24 -0400

    Added post requests

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,45 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Systrace,
+} from "react-native";
 import { styles } from "../Styles";
 import { username } from "../screens/Login";
 import logo from "../assets/logo.png";
+import axios from "axios";
+
 function Home(props) {
   const [lockstate, setLockstate] = useState(false);
+  const username = "username";
+  const password = "password";
   const changeLockState = () => {
     if (lockstate == false) {
       setLockstate(true);
       setLockStateText("lock");
+      axios
+        .post("https://p4qcydmk3c.tunnel.kundu.io/command/lock", {
+          username: username,
+          password: password,
+        })
+        .then(function (response) {
+          console.log(response);
+        });
     }
     if (lockstate == true) {
       setLockstate(false);
       setLockStateText("unlock");
+      axios
+        .post("https://p4qcydmk3c.tunnel.kundu.io/command/lock", {
+          username: username,
+          password: password,
+        })
+        .then(function (response) {
+          console.log(response);
+        });
     }
   };
   const [lockstateText, setLockStateText] = useState("unlock");
