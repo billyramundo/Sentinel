eb9b4ed4a75e80fc5f5b5c2fb081f497bb1c2bb4
Billy Ramundo <william.ramundo@duke.edu>
2021-11-08 23:10:48 -0500

    adding test functionality in friends to pull info from database

--- screens/Friends.js
+++ screens/Friends.js
@@ -1,20 +1,43 @@
 import React, { useState } from "react";
 import { styles } from "../Styles";
 import SearchField from "react-search-field";
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+import "firebase-admin";
+import { database } from "../screens/Login";
 import {
     View,
     TextInput,
+    Text,
 } from "react-native";
 
 function Friends({ navigation }) {
-
+    const [email, setEmail] = useState("");
+    const [displayEmail, setDisplayEmail] = useState("");
+    const getUser = () => {
+        firebase
+            .auth()
+            .getUserByEmail(email)
+            .then((userRecord) => {
+                setDisplayEmail(userRecord.getEmail());
+                console.log("Successfully found user");
+            })
+            .catch((error) => {
+                console.log("User does not exist")
+            });
+    };
     return (
         <View style={styles.back}>
             <View style={styles.centeredcontainer}>
                 <SearchField
-                    placeholder="Search for friends" />
+                    placeholder="Search for friends"
+                    onChange={(email) => setEmail(email)}
+                    onSearchClick={getUser} />
             </View>
-            <View style={styles.middlecontainer}></View>
+            <View style={styles.middlecontainer}>
+                <Text>{displayEmail}</Text>
+            </View>
             <View style={styles.middlecontainer}></View>
             <View style={styles.middlecontainer}></View>
         </View>
